f2e470e01ff0234375bcc4d583212c2136c5abbd
Moritz <web.github@scheve.org>
2023-11-22 06:21:08 +0100

    Add color highlight of slice areas when target is close

--- BeatNinja/Assets/Scripts/SliceArea.cs
+++ BeatNinja/Assets/Scripts/SliceArea.cs
@@ -1,19 +1,44 @@
 using System.Collections;
 using System.Collections.Generic;
 using System.Linq;
+using Unity.VisualScripting;
 using UnityEngine;
 
 [RequireComponent(typeof(CircleCollider2D))]
+[RequireComponent(typeof(SpriteRenderer))]
 public class SliceArea : MonoBehaviour
 {
     public Blade Blade;
 
     private CircleCollider2D _collider;
+    private SpriteRenderer _spriteRenderer;
 
     private void Awake()
     {
         _collider = GetComponent<CircleCollider2D>();
+        _spriteRenderer = GetComponent<SpriteRenderer>();
         if (_collider.offset != Vector2.zero) Debug.LogError("The SliceArea's collider must not use an offset!");
+    }
+
+    private void Update()
+    {
+        var nearest = GetNearestSliceable();
+        if (nearest == null) return;
+        var distance = Vector2.Distance(this.transform.position, nearest.transform.position);
+        var nDistance = distance / _collider.radius;
+        if (nDistance < 0.5f) _spriteRenderer.color = Color.red;
+        else _spriteRenderer.color = Color.white;
+    }
+
+    private Sliceable? GetNearestSliceable()
+    {
+        // Find all non-sliced sliceables
+        var sliceables = FindObjectsByType<Sliceable>(FindObjectsSortMode.None).Where(s => !s.IsSliced);
+
+        // Since we dont care about the z-axis transmuting is fine here
+        var ordered = sliceables.OrderBy(x => Vector2.Distance(x.transform.position, this.transform.position));
+        var nearest = ordered.FirstOrDefault(); // Default is null
+        return nearest;
     }
 
     public float Slice()
@@ -22,12 +47,7 @@ public class SliceArea : MonoBehaviour
         if (dir.y < 0) dir.y *= -1;
         if(!Blade.Slice(dir)) return 0;
 
-        // Find all non-sliced sliceables
-        var sliceables = FindObjectsByType<Sliceable>(FindObjectsSortMode.None).Where(s => !s.IsSliced);
-
-        // Since we dont care about the z-axis transmuting is fine here
-        var ordered = sliceables.OrderBy(x => Vector2.Distance(x.transform.position, this.transform.position));
-        var nearest = ordered.FirstOrDefault(); // Default is null
+        var nearest = GetNearestSliceable();
         if (nearest == null) return Config.MissPenalty;
         if (!_collider.OverlapPoint(nearest.transform.position)) return Config.MissPenalty;
 
